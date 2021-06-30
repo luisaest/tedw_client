6c6cf2cc24d2ba135385fbe3b34e0ddb6cc95260
luisaestm <18030102@itcelaya.edu.mx>
2021-06-29 22:41:21 -0500

    arreglo main

--- src/main.js
+++ src/main.js
@@ -1,20 +1,14 @@
 import { sync } from 'vuex-router-sync'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
-<<<<<<< HEAD
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-=======
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
->>>>>>> 623b2b1d7ad119d524c1760ddc71b7455ccb0f57
 
 Vue.config.productionTip = false
 sync(store, router);
@@ -25,8 +19,3 @@ new Vue({
   vuetify,
   render: h => h(App)
 }).$mount('#app')
-
-// Make BootstrapVue available throughout your project
-Vue.use(BootstrapVue)
-// Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin)
