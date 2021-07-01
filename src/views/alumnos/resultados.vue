a61416d5f2eaca6000cc9796a218829da1af4c82
Luis Ramirez <ramirez270198@gmail.com>
2021-06-30 20:05:19 -0500

    alertas tomando el id desde la ruta

--- src/views/alumnos/resultados.vue
+++ src/views/alumnos/resultados.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
         <Nav></Nav>
-        <div >dvfvb</div>
-        <h1 class="text-center">Resultados de encuestas y consultas {{$route.params.id}}</h1>
+        <div style="display:none;">{{this.id_user=$route.params.id}}</div>
+        <h1 class="text-center">Resultados de encuestas y consultas </h1>
         <Resultados :id_user="id_user"/>
     </div>
 </template>
@@ -13,7 +13,7 @@ import Resultados from '@/components/alumnos/ResultadosAlumno.vue'
 export default {
     name:'resultados',
     data:()=>({
-        id_user: 1,
+        id_user: 0,
     }),
     components:{
         Nav,
